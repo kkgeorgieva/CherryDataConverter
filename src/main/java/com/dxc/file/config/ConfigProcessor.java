e0ca2199d06a756d094e1bfe0b7678d33b6c72c3
Kristina Georgieva <kristina.georgieva2@dxc.com>
2023-08-16 12:24:04 +0300

    Refactored Config Processor for yaml configuration file

--- src/main/java/com/dxc/file/config/ConfigProcessor.java
+++ src/main/java/com/dxc/file/config/ConfigProcessor.java
@@ -1,54 +1,39 @@
 package com.dxc.file.config;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
 
 public class ConfigProcessor {
-	// Processes the config file and creates different properties that define the user's requirements
-	// The properties will be then created as key value pairs.
-	// Reads only categories key and value
-	// Ignores comments initialized with #
 	
-	private String fileName;
-	private List<Property> props;
 	
-	public ConfigProcessor(String fileName) {
-		this.fileName = fileName;
-		this.props = new ArrayList<>();
-		this.getProps();
-	}
-	
-	private void getProps() {
-		Properties prop = new Properties();
-		
-		try (FileInputStream fis = new FileInputStream(fileName)) {
-			prop.load(fis);
-		} catch (FileNotFoundException ex) {
-			System.out.println(ex.getMessage());
-		} catch (IOException ex) {
-			System.out.println(ex.getMessage());
-		}
-		
-		
-		
-		
-		prop.entrySet().forEach(e -> {
-			String category = e.getKey().toString().split("\\.")[0];
-			String key = e.getKey().toString().split("\\.")[1];
-			String value = e.getValue().toString();
-			
-			this.getPropsList().add(new Property(category, key, value));
-		});;
-		
-	}
 
-	public List<Property> getPropsList() {
-		return props;
-	}
-	
+	public static List<Property> parseConfig(String configFile) {
+        List<Property> properties = new ArrayList<>();
+
+        try (FileInputStream inputStream = new FileInputStream(configFile)) {
+            Yaml yaml = new Yaml();
+            Map<String, Object> configMap = yaml.load(inputStream);
+
+            for (Map.Entry<String, Object> entry : configMap.entrySet()) {
+                String category = entry.getKey();
+                Map<String, Object> section = (Map<String, Object>) entry.getValue();
+
+                for (Map.Entry<String, Object> propertyEntry : section.entrySet()) {
+                    String key = propertyEntry.getKey();
+                    String value = propertyEntry.getValue().toString();
+                    properties.add(new Property(category, key, value));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
 
 }
--- src/main/java/com/dxc/system/Converter.java
+++ src/main/java/com/dxc/system/Converter.java
@@ -1,5 +1,4 @@
 package com.dxc.system;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,56 +9,43 @@ import com.dxc.file.writer.FileWriterProvider;
 
 public class Converter {
 	// The converter will store the supported file types
-	//Throws exception when the requested file type is not supported by the converter
-	
+	// Throws exception when the requested file type is not supported by the
+	// converter
+
 	public FileReaderProvider fileReader;
 	public FileWriterProvider fileWriter;
-	
+
 	public Converter(FileReaderProvider fileReader, FileWriterProvider fileWriter) {
 		this.fileReader = fileReader;
 		this.fileWriter = fileWriter;
 	}
-	
+
 	public void convert(String inputFilePath, String outputFilePath) {
 		ArrayList<String> readFile = fileReader.readFile(outputFilePath);
-		
+
 		fileWriter.write(readFile, outputFilePath);
 	}
-	
+
 	public static void main(String[] args) {
-		
-		/* Implementation for case with Properties Class
-		 * String inputFile = args[0];
-		String outputFile = args[1];
-		String configFile = args[2];
-		
-		ConfigProcessor cf = new ConfigProcessor(configFile);
-		
-		//Read the input file type and output file type, and create appropriate reader and writer.
 
-		Converter cvt = new Converter(new CSVFileReader(cf.getPropsList()), new CSVFileWriter(cf.getPropsList()));
-		
-		cvt.convert(inputFile, outputFile);
-		*/
-		
-		String configFile = "C:\\WS\\repos\\CherryDataConvert\\configExample2.txt"; // Provide the correct path to your config file
+		// Implementation for case with Properties Class
+		//String inputFile = args[0];
+		//String outputFile = args[1];
+		//String configFile = args[2];
 
-        ConfigProcessor configProcessor = new ConfigProcessor(configFile);
-        List<Property> propsList = configProcessor.getPropsList();
+		List<Property> parseConfig = ConfigProcessor.parseConfig("configTemplate.yaml");
 
-        for (Property property : propsList) {
-            String category = property.getCategory();
-            String key = property.getKey();
-            String value = property.getValue();
+		// Read the input file type and output file type, and create appropriate reader
+		// and writer.
 
-            System.out.println("Category: " + category + ", Key: " + key + ", Value: " + value);
- 
-		
-//		
-//		for (Property pr : cf.getPropsList()) {
-//			System.out.println(pr.toString());
-//		}
-        }
+		// Converter cvt = new Converter(new CSVFileReader(cf.getPropsList()), new
+		// CSVFileWriter(cf.getPropsList()));
 
+		// cvt.convert(inputFile, outputFile);
+
+		for (Property pr : parseConfig) {
+			System.out.println(pr.toString());
+		}
 	}
+
 }
